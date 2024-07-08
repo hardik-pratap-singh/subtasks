c152e0c6aee15beefecef31733a4f335e01f8f23
hardik-pratap-singh <21bcs090@iiitdmj.ac.in>
2024-07-08 10:12:50 +0530

    second

--- Jun28th/dfs.js
+++ Jun28th/dfs.js
@@ -1,106 +1,61 @@
-let textNodes = [] ; 
-        let body = document.querySelector("body") ;
-        console.log(body.childNodes) ; 
-
-        getAllTextNodes(body) ; 
 
 
-        function getAllTextNodes(node){
-            if(node.nodeType === 3){
-                textNodes.push({node : node , parent : node.parentNode}) ;
-                return ; 
-            }
 
-            let children = Array.from(node.childNodes) ; 
-            for(let i = 0 ; i < children.length ; i++){
-                getAllTextNodes(children[i]) ;
-            }
+let textNodes = [];
+let body = document.querySelector("body");
+body.normalize()
+// console.log(body.childNodes);
+cleanNode(body); 
+getAllTextNodes(body);
+
+
+function cleanNode(node) {
+    var child;
+    for (var i = node.childNodes.length; i--;) {
+        child = node.childNodes[i];
+        // If commentt/textNode and has no non-whitespace character in it, delete it.
+        if (child.nodeType === 3 || child.nodeType === 8 && !/\S/.test(child.nodeValue)) {
+            node.removeChild(child);
+            n--;
         }
+        else {
+            cleanNode(child);
+        }
+    }
+}
 
-        console.log("textNodes  " , textNodes) ; 
+function getAllTextNodes(node) {
+    if (node.nodeType === 3) {
+        textNodes.push({ node: node, parent: node.parentNode });
+        return;
+    }
 
-        const iconSrc = '../info.svg';
-        const iconAlt = 'Icon description';
-        const targetWords = ['crazy']; 
-        // const targetWords = ['crazy', 'stupid', 'mad']; 
-        let ct = 0 ; 
+    let children = Array.from(node.childNodes);
+    for (let i = 0; i < children.length; i++) {
+        getAllTextNodes(children[i]);
+    }
+}
+
+console.log("textNodes  ", textNodes);
+
+const iconSrc = '../info.svg';
+const iconAlt = 'Icon description';
+const targetWords = ['crazy'];
+let ct = 0;
+// console.log(textNodes);
 
 
-        
-        textNodes.forEach(text => {
-            const textNode = text.node ; 
-            const parentNode = text.parent ; 
-            // console.log(text)
-            targetWords.forEach(targetWord => {
-                if(textNode.textContent.includes(targetWord)){
-                    const className = `icon-container-${targetWord}`;
-                    const parts = textNode.textContent.split(targetWord);
-                    const replacedHTML = parts.join(`${targetWord}<span class="${className}">**</span>`);
-                    console.log(replacedHTML)
-                    parentNode.innerHTML = replacedHTML ; 
-
-                    // console.log(parts)
-                    // console.log(textNode.textContent)
-                    // console.log(targetWord) ; 
-                    // console.log("hello")
-                }
-            } )
-            // targetWords.forEach(targetWord => {
-            //     if(textNode.textContent.includes(targetWord)){
-            //         const className = `icon-container-${targetWord}`;
-            //         const parts = textNode.textContent.split(targetWord);
-            //         const replacedHTML = parts.join(`${targetWord}<span class="${className}"></span>`);
-            //         parentNode.innerHTML = replacedHTML;
-            //     }
-            // })
-
-            // const iconContainers = parentNode.querySelectorAll(`.${className}`);
-
-            //         // console.log(iconContainers);
-            //         iconContainers.forEach(container => {
-            //             // console.log(container) ; 
-            //             const icon = document.createElement('img');
-            //             icon.src = iconSrc;
-            //             icon.alt = iconAlt;
-            //             container.appendChild(icon);
-
-            //             // console.log(icon)
-            //         });
-        })
-
-        let doc = document.querySelectorAll("span")
-        console.log(doc)
-        // document.querySelectorAll('*').forEach(element => {
-        //     // let element = obj.parent ; 
-        //     // console.log(element)
-        //     targetWords.forEach(targetWord => {
-                
-        //         // ct++ ; 
-        //         if (element.innerHTML.includes(targetWord)) {
-        //             const className = `icon-container-${targetWord}`;
-        //             // Split the innerHTML into parts to handle replacements
-        //             const parts = element.innerHTML.split(targetWord);
-        //             const replacedHTML = parts.join(`${targetWord}<span class="${className}"></span>`);
-
-        //             // Update the element with the replaced content
-        //             element.innerHTML = replacedHTML;
-        //             // console.log(replacedHTML)
-
-        //             // Add icon after each occurrence of the target word
-        //             const iconContainers = element.querySelectorAll(`.${className}`);
-
-        //             // console.log(iconContainers);
-        //             iconContainers.forEach(container => {
-        //                 // console.log(container) ; 
-        //                 const icon = document.createElement('img');
-        //                 icon.src = iconSrc;
-        //                 icon.alt = iconAlt;
-        //                 container.appendChild(icon);
-
-        //                 // console.log(icon)
-        //             });
-        //         }
-        //     });
-        // });
-
-        // console.log(ct)
+textNodes.forEach(text => {
+    const textNode = text.node;
+    const parentNode = text.parent;
+    // console.log(text)
+    targetWords.forEach(targetWord => {
+        if (parentNode.innerHTML.includes(targetWord)) {
+            const className = `icon-container-${targetWord}`;
+            const parts = parentNode.innerHTML.split(targetWord);
+            const replacedHTML = parts.join(`${targetWord}<span class="${className}">**</span>`);
+            // console.log(replacedHTML)
+            // parentNode.innerHTML = replacedHTML;
+        }
+    })
+})
